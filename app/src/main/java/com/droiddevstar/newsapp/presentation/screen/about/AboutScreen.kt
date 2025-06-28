39811cb07c20821034b71d5290446d08d9d494be
Dmitry Starchevsky <dstarchevskiy@kamaz.digital>
2025-06-28 15:06:39 +0300

    about 2

--- app/src/main/java/com/droiddevstar/newsapp/presentation/screen/about/AboutScreen.kt
+++ app/src/main/java/com/droiddevstar/newsapp/presentation/screen/about/AboutScreen.kt
@@ -1,8 +1,22 @@
 package com.droiddevstar.newsapp.presentation.screen.about
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.droiddevstar.newsapp.R
 import com.droiddevstar.newsapp.presentation.navigation.Screen
 
@@ -10,5 +24,36 @@ import com.droiddevstar.newsapp.presentation.navigation.Screen
 fun AboutScreen(
     onNavigate: (Screen) -> Unit
 ) {
-    Text(text = stringResource(id = R.string.about))
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(top = 16.dp, start = 16.dp, end = 16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = stringResource(id = R.string.about),
+            fontSize = 30.sp
+        )
+
+        Text(
+            text = stringResource(id = R.string.my_name),
+            modifier = Modifier.padding(top = 16.dp),
+            fontSize = 26.sp
+        )
+
+        Text(
+            text = stringResource(id = R.string.android_developer),
+            color = Color.Blue,
+            fontSize = 26.sp,
+        )
+
+        Image(
+            modifier = Modifier.fillMaxWidth()
+                .padding(top = 16.dp),
+            painter = painterResource(R.drawable.dima_photo),
+            contentDescription = "Dima photo",
+            contentScale = ContentScale.FillWidth
+        )
+    }
+
 }
