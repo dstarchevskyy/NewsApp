7eb8741305298d879b3f3ba4822e98e21c83e52f
Dmitry Starchevsky <dstarchevskiy@kamaz.digital>
2025-06-29 16:12:13 +0300

    add mem 1

--- app/src/main/java/com/droiddevstar/newsapp/presentation/screen/about/AboutScreen.kt
+++ app/src/main/java/com/droiddevstar/newsapp/presentation/screen/about/AboutScreen.kt
@@ -2,7 +2,9 @@ package com.droiddevstar.newsapp.presentation.screen.about
 
 import android.content.Context
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -56,13 +58,21 @@ fun AboutScreen(
             fontSize = 26.sp,
         )
 
-        Image(
+        Box(
             modifier = Modifier.fillMaxWidth()
-                .padding(top = 16.dp),
-            painter = painterResource(R.drawable.dima_photo),
-            contentDescription = "Dima photo",
-            contentScale = ContentScale.FillWidth
-        )
+                .background(color = Color.LightGray),
+            contentAlignment = Alignment.Center
+        ) {
+            Image(
+                modifier = Modifier.fillMaxWidth()
+                    .padding(top = 16.dp),
+                painter = painterResource(R.drawable.dima_photo),
+                contentDescription = "Dima photo",
+                contentScale = ContentScale.FillWidth
+            )
+
+            BalabanovView()
+        }
 
         Row(modifier = Modifier
             .fillMaxWidth()
---
+++ app/src/main/java/com/droiddevstar/newsapp/presentation/screen/about/BalabanovView.kt
@@ -0,0 +1,66 @@
+package com.droiddevstar.newsapp.presentation.screen.about
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import com.droiddevstar.newsapp.R
+
+@Composable
+fun BalabanovView(
+) {
+    Column(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ){
+        val arialBlack = FontFamily(
+            Font(R.font.arial_black) // Make sure the filename matches exactly
+        )
+
+        Text(
+            text = stringResource(
+                id = R.string.director
+            ),
+            Modifier.background(
+                color = colorResource(id = R.color.transparent_light_grey)
+            ),
+            style = TextStyle(
+                fontFamily = arialBlack,
+                fontSize = 20.sp
+            )
+        )
+
+        Text(
+            text = stringResource(
+                id = R.string.balabanov
+            ),
+            textAlign = TextAlign.Center,
+            color = Color.Red,
+            style = TextStyle(
+                fontFamily = arialBlack,
+                fontSize = 30.sp
+            )
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun BalabanovViewPreview() {
+    BalabanovView()
+}
