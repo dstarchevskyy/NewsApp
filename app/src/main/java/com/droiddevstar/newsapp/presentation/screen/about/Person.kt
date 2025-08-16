3363b8480b5bcc631862af2ee0845c327081f8be
Dmitry Starchevsky <dstarchevskiy@kamaz.digital>
2025-08-16 16:07:01 +0300

    add GameScreen

--- app/src/main/java/com/droiddevstar/newsapp/presentation/screen/about/Person.kt
+++
@@ -1,9 +0,0 @@
-package com.droiddevstar.newsapp.presentation.screen.about
-
-class Person(val name: String) {
-    init {
-        println("name: $name")
-    }
-
-    constructor(val firstName: String, val lastName: String) : this(firstName)
-}
---
+++ app/src/main/java/com/droiddevstar/newsapp/presentation/screen/game/GameScreen.kt
@@ -0,0 +1,20 @@
+package com.droiddevstar.newsapp.presentation.screen.game
+
+import androidx.compose.foundation.background
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun GameScreen() {
+    Text(modifier = Modifier.background(color = Color.Red), text = "GameScreen")
+
+}
+
+@Composable
+@Preview
+fun GameScreenPreview() {
+    GameScreen()
+}
